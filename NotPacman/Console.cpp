d083d8fd039d2b5244b26234d2cdb2dd6c37c86e
Krystian S <sdkrystian@gmail.com>
2018-04-09 21:40:36 -0400

    Updated
    Added progress bar among other optimizations

--- NotPacman/Console.cpp
+++ NotPacman/Console.cpp
@@ -2,17 +2,16 @@
 #include "Console.h"
 #include <iostream>
 
-HANDLE Console::console_handle_;
-
 void Console::Clear()
 {
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
   COORD topLeft = { 0, 0 };
   CONSOLE_SCREEN_BUFFER_INFO screen;
   DWORD written;
-  GetConsoleScreenBufferInfo(console_handle_, &screen);
-  FillConsoleOutputCharacterA(console_handle_, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
-  FillConsoleOutputAttribute(console_handle_, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
-  SetConsoleCursorPosition(console_handle_, topLeft);
+  GetConsoleScreenBufferInfo(handle, &screen);
+  FillConsoleOutputCharacterA(handle, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+  FillConsoleOutputAttribute(handle, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+  SetConsoleCursorPosition(handle, topLeft);
 }
 
 void Console::Resize(int x, int y)
@@ -23,60 +22,92 @@ void Console::Resize(int x, int y)
   MoveWindow(console, rect.left, rect.top, x, y, TRUE);
 }
 
-void Console::SetTitle(std::string title)
+void Console::SetCursorPosition(short x, short y)
+{
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD pos = { x, y };
+  SetConsoleCursorPosition(handle, pos);
+}
+
+COORD Console::GetCursorPosition()
+{
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO screen;
+  GetConsoleScreenBufferInfo(handle, &screen);
+  return screen.dwCursorPosition;
+}
+
+void Console::ShowConsoleCursor(bool show)
+{
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_CURSOR_INFO cursor;
+  GetConsoleCursorInfo(handle, &cursor);
+  cursor.bVisible = show;
+  SetConsoleCursorInfo(handle, &cursor);
+}
+
+COORD Console::GetSize()
+{
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO screen;
+  GetConsoleScreenBufferInfo(handle, &screen);
+  return screen.dwSize;
+}
+
+void Console::SetTitle(const std::string& title)
 {
   SetConsoleTitle(std::wstring(title.begin(), title.end()).c_str());
 }
 
 void Console::SetColor(Color color)
 {
-  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-  SetConsoleTextAttribute(console_handle_, (WORD)color);
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(handle, (WORD)color);
 }
 
-void Console::Write(std::string text, Color color)
+void Console::Write(const std::string& text, Color color)
 {
   SetColor(color);
   std::cout << text;
 }
 
-void Console::Write(std::string text)
+void Console::Write(const std::string& text)
 {
   Console::Write(text, Color::WHITE);
 }
 
-void Console::WriteCentered(std::string text, Color color)
+void Console::WriteCentered(const std::string& text, Color color)
 {
   Console::Write(CenterString(text), color);
 }
 
-void Console::WriteCentered(std::string text)
+void Console::WriteCentered(const std::string& text)
 {
   Console::WriteCentered(text, Color::WHITE);
 }
 
-void Console::WriteLine(std::string text, Color color)
+void Console::WriteLine(const std::string& text, Color color)
 {
   Write(text, color);
   std::cout << std::endl;
 }
 
-void Console::WriteLine(std::string text)
+void Console::WriteLine(const std::string& text)
 {
   Console::WriteLine(text, Color::WHITE);
 }
 
-void Console::WriteLineCentered(std::string text, Color color)
+void Console::WriteLineCentered(const std::string& text, Color color)
 {
   Console::WriteLine(CenterString(text), color);
 }
 
-void Console::WriteLineCentered(std::string text)
+void Console::WriteLineCentered(const std::string& text)
 {
   Console::WriteLineCentered(text, Color::WHITE);
 }
 
-void Console::WriteList(std::vector<std::string> text, Color color)
+void Console::WriteList(const std::vector<std::string>& text, Color color)
 {
   for (const std::string& s : text)
   {
@@ -84,7 +115,7 @@ void Console::WriteList(std::vector<std::string> text, Color color)
   }
 }
 
-void Console::WriteList(std::vector<std::string> text)
+void Console::WriteList(const std::vector<std::string>& text)
 {
   for (const std::string& s : text)
   {
@@ -92,7 +123,7 @@ void Console::WriteList(std::vector<std::string> text)
   }
 }
 
-void Console::WriteListCentered(std::vector<std::string> text, Color color)
+void Console::WriteListCentered(const std::vector<std::string>& text, Color color)
 {
   for (const std::string& s : text)
   {
@@ -100,7 +131,7 @@ void Console::WriteListCentered(std::vector<std::string> text, Color color)
   }
 }
 
-void Console::WriteListCentered(std::vector<std::string> text)
+void Console::WriteListCentered(const std::vector<std::string>& text)
 {
   for (const std::string& s : text)
   {
@@ -115,34 +146,25 @@ std::string Console::ReadLine()
   return input;
 }
 
-std::string Console::ReadLinePrompt(std::string prompt, Color color)
+std::string Console::ReadLinePrompt(const std::string& prompt, Color color)
 {
   Console::Write(prompt, color);
   return Console::ReadLine();
 }
 
-std::string Console::ReadLinePrompt(std::string prompt)
+std::string Console::ReadLinePrompt(const std::string& prompt)
 {
   Console::Write(prompt);
   return Console::ReadLine();
 }
 
-void Console::SetConsoleHandle(HANDLE handle)
+std::string Console::CenterString(const std::string& str)
 {
-  console_handle_ = handle;
-}
-
-void Console::CloseConsoleHandle()
-{
-  CloseHandle(console_handle_);
-}
-
-std::string Console::CenterString(std::string str)
-{
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO screen;
   int columns;
-  GetConsoleScreenBufferInfo(console_handle_, &csbi);
-  columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+  GetConsoleScreenBufferInfo(handle, &screen);
+  columns = screen.srWindow.Right - screen.srWindow.Left + 1;
   std::string output;
   int padding = (columns / 2) - (str.size() / 2);
   for (int i = 0; i < padding; ++i)
